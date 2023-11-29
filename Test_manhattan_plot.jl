be66bcd98cebee73157e1a62f18cc278b4af0e19
Sai Sruthi <59644095+Saisruthiamirthaganesh@users.noreply.github.com>
2023-11-28 16:46:51 -0800

    Update Test_manhattan_plot.jl

--- Test_manhattan_plot.jl
+++ Test_manhattan_plot.jl
@@ -1,4 +1,5 @@
-using GeneticsMakie, CairoMakie, DataFrames, CSV
+using GeneticsMakie, CairoMakie, DataFrames, CSV, Arrow
 summary_stats= CSV.read("iPSYCH-PGC_ASD_Nov2017",  DataFrame)
 GeneticsMakie.mungesumstats!(summary_stats)
 loci = GeneticsMakie.findgwasloci(summary_stats)
+Arrow.write("/Users/admin/asd_analysis/test_analysis_manplot/summary_stats.arrow", summary_stats)
